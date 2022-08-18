9c46d4d3d9f5167cb875b99cdeb6136c2e0a2c75
Caleb Koehn <calebkoehn99@outlook.com>
2022-08-17 21:04:20 -0500

    updated w/Save and Delete

--- public/script.js
+++ public/script.js
@@ -1,6 +1,10 @@
 const textEditor = document.querySelector(".text-editor");
 const preview = document.querySelector(".preview");
+const fileName = document.querySelector(".file");
+const saveChange = document.querySelector(".header-button");
+const deleteButton = document.querySelector(".delete-button");
 const converter = new showdown.Converter();
+
 
 const renderPreview = (value) => {
     const html = converter.makeHtml(value);
@@ -9,14 +13,42 @@ const renderPreview = (value) => {
 
 textEditor.addEventListener('keyup',evt => {
     const {value} = evt.target;
-    window.localStorage.setItem('markdown', value);
+    // window.localStorage.setItem('markdown', value);
     
     renderPreview(value);
 });
+fileName.addEventListener('keyup', evt => {
+    const {value} = evt.target;
+    window.localStorage.setItem('file',value);
+
+    renderFileName(value);
+});
+
+saveChange.addEventListener('click', evt => {
+    const {value} = evt.target;
+    window.localStorage.setItem('markdown', value);
+})
 
 const storedMarkdown = window.localStorage.getItem('markdown');
 
+
 if (storedMarkdown) {
-    textEditor.value = storedMarkdown;
+    
+    saveChange.value = storedMarkdown;
     renderPreview(storedMarkdown);
-}
+};
+
+const storedFileName = window.localStorage.getItem('file');
+
+if (storedFileName) {
+    fileName.value = storedFileName;
+    renderFileName(storedFileName);
+};
+
+// deleteButton = window.localStorage.clear();
+
+deleteButton.addEventListener('click', evt => {
+    
+    window.localStorage.clear();
+});
+
